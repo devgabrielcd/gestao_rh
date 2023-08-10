e651c7696d3702b62b86ff244dd09cb3e45c6c23
gabriel <ggabrielcd@hotmail.com>
2023-08-09 21:18:25 -0300

    fixing the last commit

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -145,10 +145,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR / "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 
